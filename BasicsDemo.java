cea2b08f2c4906fcc143f8243161874796a6af04
Abdelfattah Altaeb <42971313+aftiyah94@users.noreply.github.com>
2019-03-01 15:53:25 +0200

    literals demo

--- BasicsDemo.java
+++ BasicsDemo.java
@@ -23,6 +23,17 @@ public class BasicsDemo {
         System.out.println("intChar: " + intChar);
         int intUnderscore = 1_23_456;
         System.out.println("intUnderscore: " + intUnderscore);
+
+        char charA = 'A';
+        System.out.println("charA: " + charA);
+        char charInt = 65;
+        System.out.println("charInt: " + charInt);
+        char charUnicode1 = '\u0041'; // 4 * (16 power 1 )+ 1 * (16 power 0)
+        System.out.println("charUnicode1: " + charUnicode1);
+        char charUnicode2 = 0x41;     // 4 * (16 power 1 )+ 1 * (16 power 0)
+        System.out.println("charUnicode2: " + charUnicode2);
+        char charBinary = 0b01000001; // binary corresponding to decimal 65
+        System.out.println("charBinary: " + charBinary);
     }
 
     public static void main(String[] args) {
