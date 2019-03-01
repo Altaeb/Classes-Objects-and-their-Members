9924183c681cf91f8706ef817a193accb0411959
Abdelfattah Altaeb <42971313+aftiyah94@users.noreply.github.com>
2019-03-01 15:50:12 +0200

    holds decimal equivalent

--- BasicsDemo.java
+++ BasicsDemo.java
@@ -2,11 +2,6 @@ package sample.BasicsDemo;
 
 public class BasicsDemo {
 
-    public static void main(String[] args) {
-        // Language Basics 1
-        print();
-    }
-    // Adapted from http://www.ntu.edu.sg/home/ehchua/programming/java/J1a_Introduction.html
     static void print() {
         System.out.println("\n\nInside print ...");
         System.out.println("Hello, world!!"); // Advance cursor to beginning of next line
@@ -17,4 +12,22 @@ public class BasicsDemo {
         System.out.print("world!!");
     }
 
+    static void primitives() {
+        System.out.println("\n\nInside primitives ...");
+        // literals demo
+        int intHex = 0x0041;
+        System.out.println("intHex: " + intHex);
+        int intBinary = 0b01000001;
+        System.out.println("intBinary: " + intBinary);
+        int intChar = 'A'; // holds decimal equivalent, which is 65
+        System.out.println("intChar: " + intChar);
+        int intUnderscore = 1_23_456;
+        System.out.println("intUnderscore: " + intUnderscore);
+    }
+
+    public static void main(String[] args) {
+        // Language Basics 1
+        //print();
+        primitives();
+    }
 }
