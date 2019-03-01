2f9440c3d740138a80e8fc83b8fd759041e3be94
Abdelfattah Altaeb <42971313+aftiyah94@users.noreply.github.com>
2019-03-01 16:50:14 +0200

    typeCasting

--- BasicsDemo.java
+++ BasicsDemo.java
@@ -1,6 +1,7 @@
-package sample.BasicsDemo;
+package sample;
 
 public class BasicsDemo {
+
 
     static void print() {
         System.out.println("\n\nInside print ...");
@@ -36,9 +37,39 @@ public class BasicsDemo {
         System.out.println("charBinary: " + charBinary);
     }
 
+    static void typeCasting() {
+        System.out.println("\nInside typeCasting ...");
+        // Explicit casting
+        long y = 42;
+        //int x = y;
+        int x = (int)y;
+
+        // Information loss due to out-of-range assignment
+        byte narrowdByte = (byte)123456;
+        System.out.println("narrowdByte: " + narrowdByte);
+
+        // Truncation
+        int iTruncated = (int)0.99;
+        System.out.println("iTruncated: " + iTruncated);
+
+        // Implicit cast (int to long)
+        y = x;
+
+        // Implicit cast (char to int)
+        char cChar = 'A';
+        int iInt = cChar;
+        System.out.println("iInt: " + iInt);
+
+        // byte to char using an explicit cast
+        byte bByte = 65;
+        cChar = (char)bByte; // special conversion (widening from byte --> int followed by narrowing from int --> char)
+        System.out.println("cChar: " + cChar);
+    }
+
     public static void main(String[] args) {
         // Language Basics 1
         //print();
-        primitives();
+        //primitives();
+        typeCasting();
     }
 }
