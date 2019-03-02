96dcd49aae6b7cd5042eb8eadb3b8342d690bb91
Abdelfattah Altaeb <42971313+aftiyah94@users.noreply.github.com>
2019-03-02 15:15:03 +0200

    three Dimensional Arrays

--- BasicsDemo.java
+++ BasicsDemo.java
@@ -73,11 +73,44 @@ public class BasicsDemo {
         System.out.println("myArray[7]: " + myArray[7]);
     }
 
+    static void threeDimensionalArrays() {
+        System.out.println("\nInside threeDimensionalArrays ...");
+        int[][][] unitsSold = new int[][][]{
+                { // New York
+                        {0,0,0,0}, // Jan
+                        {0,0,0,0}, // Feb
+                        {0,0,0,0}, // Mar
+                        {0,850,0,0}// Apr
+                },
+                { // San Francisco
+                        {0,0,0,0}, // Jan
+                        {0,0,0,0}, // Feb
+                        {0,0,0,0}, // Mar
+                        {0,0,0,0}  // Apr
+                },
+                {
+                        {0,0,0,0},
+                        {0,0,0,0},
+                        {0,0,0,0},
+                        {0,0,0,0}
+                },
+                {
+                        {0,0,0,0},
+                        {0,0,0,0},
+                        {0,0,0,0},
+                        {0,0,0,0}
+                }
+        };
+
+        System.out.println("unitsSold[0][3][1]: " + unitsSold[0][3][1]);
+    }
+
     public static void main(String[] args) {
         // Language Basics 1
         //print();
         //primitives();
         //typeCasting();
-        arrays();
+        //arrays();
+        threeDimensionalArrays();
     }
 }
