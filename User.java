026061b851b5b3fe9f9601c2be4ee9796ccf2931
Abdelfattah Altaeb <42971313+aftiyah94@users.noreply.github.com>
2019-03-03 09:59:49 +0200

    Example about constractor

---
+++ User.java
@@ -0,0 +1,20 @@
+class User {
+     int id;
+     String name;
+     int salary;
+
+     User(int userId, String userName) { 
+	     id = userId;
+         name = userName;
+     }
+	 
+	 User(int userId, String userName, int userSalary) { 
+         this(userId, userName);
+		 salary = userSalary;
+     }   
+	 
+	 public static void main(String[] args) {
+		User instructor = new User(1002, "Dheeru", 50000);
+		System.out.println("Name: " + instructor.name);
+	 }
+}
