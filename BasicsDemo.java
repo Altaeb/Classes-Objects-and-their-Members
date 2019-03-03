025bb4521ae5c12e2c1075d71f3dcbc33f46abd5
Abdelfattah Altaeb <42971313+aftiyah94@users.noreply.github.com>
2019-03-03 17:39:42 +0200

    Bitwise Operators

--- BasicsDemo.java
+++ BasicsDemo.java
@@ -119,6 +119,27 @@
 	  System.out.println("(char1 - '3'): " + (char1 - '3')); // char1 & '3' are promoted to ints	
 	  System.out.println("('a' + 'b'): " + ('a' +'b')); // 'a' & 'b' are promoted to ints and the respective equivalents 97 & 98 are added
     }
+	
+	static void bitwiseOperators() {
+	    System.out.println("\nInside bitwiseOperators ...");
+	    int x = 1;
+		int y = 3;
+		
+		System.out.println("x & y: " + (x & y));
+		System.out.println("x | y: " + (x | y));
+		System.out.println("x ^ y: " + (x ^ y));
+		System.out.println("~x: " + (~x));
+		System.out.println("true & false: " + (true & false));
+		 
+		char c1 = 'a'; // U+0061 --> 0110 0001
+		char c2 = 'b'; // U+0062 --> 0110 0010
+		System.out.println("c1 | c2: " + (c1 | c2)); // 0110 0011 --> 99 in decimal		
+		
+		// Since bitwise work only on Integer types, following will not compile
+		//double d1 = 3.14;
+		//double d2 = 5.15;
+		//System.out.println("d1 | d2: " + (d1 | d2));
+  }
 		
 	public static void main(String[] args) {	
 	  // Language Basics 1
@@ -130,6 +151,7 @@
       /*varargsOverload(true, 1, 2, 3);
       varargsOverload(true, 1, 2, 3, 4, 5, 6, 7, 8);	  
       varargsOverload(true);*/
-	  charTypePromotion();
+	  //charTypePromotion();
+	  bitwiseOperators();
     }       
   }
